29fac21cedc1820ef4b58a111deb215efe149f6e
Paul Coignac <paulcoignac@gmail.com>
2025-04-15 12:12:23 +0200

    Add density and type

--- go.mod
+++ go.mod
@@ -3,11 +3,19 @@ module go_project
 go 1.24
 
 require (
-	github.com/ebitengine/gomobile v0.0.0-20240911145611-4856209ac325 // indirect
+	github.com/ebitenui/ebitenui v0.6.2
+	github.com/hajimehoshi/ebiten/v2 v2.8.7
+	golang.org/x/image v0.25.0
+)
+
+require (
+	github.com/ebitengine/gomobile v0.0.0-20250209143333-6071a2a2351c // indirect
 	github.com/ebitengine/hideconsole v1.0.0 // indirect
-	github.com/ebitengine/purego v0.8.0 // indirect
-	github.com/hajimehoshi/ebiten/v2 v2.8.7 // indirect
+	github.com/ebitengine/purego v0.8.2 // indirect
+	github.com/go-text/typesetting v0.3.0 // indirect
 	github.com/jezek/xgb v1.1.1 // indirect
-	golang.org/x/sync v0.8.0 // indirect
-	golang.org/x/sys v0.25.0 // indirect
+	golang.org/x/exp v0.0.0-20250305212735-054e65f0b394 // indirect
+	golang.org/x/sync v0.13.0 // indirect
+	golang.org/x/sys v0.32.0 // indirect
+	golang.org/x/text v0.23.0 // indirect
 )
--- main.go
+++ main.go
@@ -1,12 +1,18 @@
 package main
 
 import (
+	"bytes"
 	"fmt"
+	image2 "github.com/ebitenui/ebitenui/image"
 	"github.com/hajimehoshi/ebiten/v2/ebitenutil"
+	"github.com/hajimehoshi/ebiten/v2/text/v2"
+	"golang.org/x/image/font/gofont/goregular"
 	"image/color"
 	"log"
 	"math/rand"
 
+	"github.com/ebitenui/ebitenui"
+	"github.com/ebitenui/ebitenui/widget"
 	"github.com/hajimehoshi/ebiten/v2"
 )
 
@@ -16,12 +22,14 @@ const (
 	cellSize     = 5
 	gridSize     = screenWidth / cellSize
 	deltaTime    = 1.0 / 60.0
-	lastUpdate   = 0.0
 )
 
 type Game struct {
-	grid [gridSize][gridSize]Cell
+	grid             [gridSize][gridSize]Cell
+	ui               *ebitenui.UI
+	selectedCellType CellType
 }
+
 type CellType int64
 
 const (
@@ -32,18 +40,65 @@ const (
 )
 
 type Cell struct {
-	physic   func(x int, y int, g *Game) // Physic function
+	physic   func(x int, y int, g *Game)
 	cellType CellType
 	color    color.Color
+	liquid   bool
+	density  int
+}
+
+type resources struct {
+	buttonImage *widget.ButtonImage
+	font        text.Face
+	textColor   *widget.ButtonTextColor
+	padding     widget.Insets
+}
+
+func newResources() *resources {
+	idle := image2.NewNineSliceColor(color.NRGBA{R: 0x33, G: 0x33, B: 0x33, A: 0xff})
+	hover := image2.NewNineSliceColor(color.NRGBA{R: 0x44, G: 0x44, B: 0x44, A: 0xff})
+	pressed := image2.NewNineSliceColor(color.NRGBA{R: 0x22, G: 0x22, B: 0x22, A: 0xff})
+
+	font, _ := loadFont(10)
+	return &resources{
+		buttonImage: &widget.ButtonImage{
+			Idle:    idle,
+			Hover:   hover,
+			Pressed: pressed,
+		},
+		textColor: &widget.ButtonTextColor{
+			Idle: color.NRGBA{R: 0xdf, G: 0xf4, B: 0xff, A: 0xff},
+		},
+		font: font,
+		padding: widget.Insets{
+			Left:   30,
+			Right:  30,
+			Top:    10,
+			Bottom: 10,
+		},
+	}
 }
 
 func (g *Game) Update() error {
+	g.ui.Update()
+
 	if ebiten.IsMouseButtonPressed(ebiten.MouseButtonLeft) {
 		x, y := ebiten.CursorPosition()
-		cellX := x / cellSize
-		cellY := y / cellSize
-		if cellX >= 0 && cellX < gridSize && cellY >= 0 && cellY < gridSize {
-			g.grid[cellY][cellX] = NewSandCell()
+		if x >= 100 {
+			cellX := x / cellSize
+			cellY := y / cellSize
+			if cellX >= 0 && cellX < gridSize && cellY >= 0 && cellY < gridSize {
+				switch g.selectedCellType {
+				case Sand:
+					g.grid[cellY][cellX] = NewSandCell()
+				case Water:
+					g.grid[cellY][cellX] = NewWaterCell()
+				case Air:
+					g.grid[cellY][cellX] = NewAirCell()
+				case Metal:
+					g.grid[cellY][cellX] = NewMetalCell()
+				}
+			}
 		}
 	}
 	return nil
@@ -51,25 +106,29 @@ func (g *Game) Update() error {
 
 func (g *Game) Draw(screen *ebiten.Image) {
 	screen.Fill(color.Black)
-	ebiten.SetVsyncEnabled(false)
-	ebitenutil.DebugPrint(screen, fmt.Sprintf("FPS: %0.2f", ebiten.ActualFPS()))
+
 	for y := 0; y < gridSize; y++ {
 		for x := 0; x < gridSize; x++ {
 			g.grid[y][x].physic(x, y, g)
 		}
 	}
+
 	for y := 0; y < gridSize; y++ {
 		for x := 0; x < gridSize; x++ {
 			cell := g.grid[y][x]
 			if cell.cellType != Air {
 				rect := ebiten.NewImage(cellSize, cellSize)
-				rect.Fill(cell.color) // rouge
+				rect.Fill(cell.color)
 				op := &ebiten.DrawImageOptions{}
 				op.GeoM.Translate(float64(x*cellSize), float64(y*cellSize))
 				screen.DrawImage(rect, op)
 			}
 		}
 	}
+
+	g.ui.Draw(screen)
+	ebiten.SetVsyncEnabled(false)
+	ebitenutil.DebugPrint(screen, fmt.Sprintf("FPS: %0.2f", ebiten.ActualFPS()))
 }
 
 func (g *Game) Layout(outsideWidth, outsideHeight int) (int, int) {
@@ -78,14 +137,84 @@ func (g *Game) Layout(outsideWidth, outsideHeight int) (int, int) {
 
 func main() {
 	ebiten.SetWindowSize(screenWidth, screenHeight)
-	ebiten.SetWindowTitle("Grille Pixel Simulation")
-	if err := ebiten.RunGame(&Game{
-		grid: initGrid(),
-	}); err != nil {
+	ebiten.SetWindowTitle("Pixel Sand Simulation")
+
+	game := &Game{
+		grid:             initGrid(),
+		selectedCellType: Sand,
+	}
+	game.setupUI()
+
+	if err := ebiten.RunGame(game); err != nil {
 		log.Fatal(err)
 	}
 }
 
+func (g *Game) setupUI() {
+	res := newResources()
+
+	sandButton := widget.NewButton(
+		widget.ButtonOpts.WidgetOpts(widget.WidgetOpts.LayoutData(widget.RowLayoutData{Stretch: true})),
+		widget.ButtonOpts.Image(res.buttonImage),
+		widget.ButtonOpts.Text("Sand", res.font, res.textColor),
+		widget.ButtonOpts.TextPadding(res.padding),
+		widget.ButtonOpts.ClickedHandler(func(*widget.ButtonClickedEventArgs) {
+			g.selectedCellType = Sand
+		}),
+	)
+
+	waterButton := widget.NewButton(
+		widget.ButtonOpts.WidgetOpts(widget.WidgetOpts.LayoutData(widget.RowLayoutData{Stretch: true})),
+		widget.ButtonOpts.Image(res.buttonImage),
+		widget.ButtonOpts.Text("Water", res.font, res.textColor),
+		widget.ButtonOpts.TextPadding(res.padding),
+		widget.ButtonOpts.ClickedHandler(func(*widget.ButtonClickedEventArgs) {
+			g.selectedCellType = Water
+		}),
+	)
+
+	airButton := widget.NewButton(
+		widget.ButtonOpts.WidgetOpts(widget.WidgetOpts.LayoutData(widget.RowLayoutData{Stretch: true})),
+		widget.ButtonOpts.Image(res.buttonImage),
+		widget.ButtonOpts.Text("Air", res.font, res.textColor),
+		widget.ButtonOpts.TextPadding(res.padding),
+		widget.ButtonOpts.ClickedHandler(func(*widget.ButtonClickedEventArgs) {
+			g.selectedCellType = Air
+		}),
+	)
+
+	metalButton := widget.NewButton(
+		widget.ButtonOpts.WidgetOpts(widget.WidgetOpts.LayoutData(widget.RowLayoutData{Stretch: true})),
+		widget.ButtonOpts.Image(res.buttonImage),
+		widget.ButtonOpts.Text("Metal", res.font, res.textColor),
+		widget.ButtonOpts.TextPadding(res.padding),
+		widget.ButtonOpts.ClickedHandler(func(*widget.ButtonClickedEventArgs) {
+			g.selectedCellType = Metal
+		}),
+	)
+
+	buttonContainer := widget.NewContainer(
+		widget.ContainerOpts.Layout(widget.NewRowLayout(
+			widget.RowLayoutOpts.Direction(widget.DirectionVertical),
+			widget.RowLayoutOpts.Spacing(10),
+		),
+		), widget.ContainerOpts.WidgetOpts(widget.WidgetOpts.MinSize(100, 0)))
+
+	buttonContainer.AddChild(sandButton)
+	buttonContainer.AddChild(waterButton)
+	buttonContainer.AddChild(airButton)
+	buttonContainer.AddChild(metalButton)
+
+	rootContainer := widget.NewContainer(
+		widget.ContainerOpts.Layout(widget.NewAnchorLayout()),
+	)
+	rootContainer.AddChild(buttonContainer)
+	g.ui = &ebitenui.UI{
+		Container: rootContainer,
+	}
+}
+
+// Remaining original code (initGrid, cell types, physics) unchanged...
 func initGrid() [gridSize][gridSize]Cell {
 	grid := [gridSize][gridSize]Cell{}
 	for y := 0; y < gridSize; y++ {
@@ -105,23 +234,26 @@ func NewSandCell() Cell {
 	return Cell{
 		physic: func(x int, y int, g *Game) {
 			if y+1 < gridSize {
-				if g.grid[y+1][x].cellType == Air {
-					g.grid[y+1][x] = g.grid[y][x]
-					g.grid[y][x] = NewAirCell()
-				}
-				if x-1 >= 0 && g.grid[y+1][x-1].cellType == Air {
-					g.grid[y+1][x-1] = g.grid[y][x]
-					g.grid[y][x] = NewAirCell()
-				}
-				if x+1 < gridSize && g.grid[y+1][x+1].cellType == Air {
-					g.grid[y+1][x+1] = g.grid[y][x]
-					g.grid[y][x] = NewAirCell()
+				cell := g.grid[y][x]
+				if canSwitchCell(cell, g.grid[y+1][x]) {
+					copyOfCell := g.grid[y+1][x]
+					g.grid[y+1][x] = cell
+					g.grid[y][x] = copyOfCell
+				} else if x-1 >= 0 && canSwitchCell(cell, g.grid[y+1][x-1]) {
+					copyOfCell := g.grid[y+1][x-1]
+					g.grid[y+1][x-1] = cell
+					g.grid[y][x] = copyOfCell
+				} else if x+1 < gridSize && canSwitchCell(cell, g.grid[y+1][x+1]) {
+					copyOfCell := g.grid[y+1][x+1]
+					g.grid[y+1][x+1] = cell
+					g.grid[y][x] = copyOfCell
 				}
 			}
-
 		},
 		cellType: Sand,
-		color:    colors[rand.Intn(len(colors))], // yellow
+		liquid:   false,
+		density:  10,
+		color:    colors[rand.Intn(len(colors))],
 	}
 }
 
@@ -133,42 +265,55 @@ func NewWaterCell() Cell {
 	}
 	return Cell{
 		physic: func(x int, y int, g *Game) {
-			if y+1 < gridSize {
-				if g.grid[y+1][x].cellType == Air {
-					g.grid[y+1][x] = g.grid[y][x]
-					g.grid[y][x] = NewAirCell()
-				}
+			if y+1 < gridSize && g.grid[y+1][x].cellType == Air {
+				g.grid[y+1][x] = g.grid[y][x]
+				g.grid[y][x] = NewAirCell()
 			}
-
 		},
-		color:    colors[rand.Intn(len(colors))], // yellow
 		cellType: Water,
+		liquid:   true,
+		density:  9,
+		color:    colors[rand.Intn(len(colors))],
 	}
 }
 
 func NewAirCell() Cell {
-	colors := []color.Color{
-		color.RGBA{0, 0, 0, 255},
-	}
 	return Cell{
-		physic: func(x int, y int, g *Game) {
-			// Do nothing for air cells
-		},
+		physic:   func(x int, y int, g *Game) {},
 		cellType: Air,
-		color:    colors[rand.Intn(len(colors))], // yellow
+		liquid:   false,
+		density:  0,
+		color:    color.RGBA{0, 0, 0, 255},
 	}
 }
+
 func NewMetalCell() Cell {
-	colors := []color.Color{
-		color.RGBA{50, 50, 50, 255},
-		color.RGBA{60, 60, 60, 255},
-		color.RGBA{70, 70, 70, 255},
-	}
 	return Cell{
 		physic: func(x int, y int, g *Game) {
-			// Do nothing for air cells
 		},
 		cellType: Metal,
-		color:    colors[rand.Intn(len(colors))], // yellow
+		liquid:   false,
+		density:  9999,
+		color:    color.RGBA{128, 128, 128, 255},
 	}
+}
+
+func loadFont(size float64) (text.Face, error) {
+	s, err := text.NewGoTextFaceSource(bytes.NewReader(goregular.TTF))
+	if err != nil {
+		log.Fatal(err)
+		return nil, err
+	}
+
+	return &text.GoTextFace{
+		Source: s,
+		Size:   size,
+	}, nil
+}
+
+func canSwitchCell(origin Cell, target Cell) bool {
+	cellTypeDifferent := target.cellType != origin.cellType
+	hasOneLiquid := target.liquid || origin.liquid
+	targetDensityIsInferior := target.density < origin.density
+	return cellTypeDifferent && (target.cellType == Air || (hasOneLiquid && targetDensityIsInferior))
 }
